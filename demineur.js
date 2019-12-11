97777cc2c5684adb204b291e016120d364df682b
Dexeat <36676003+Dexeat@users.noreply.github.com>
2019-12-11 17:46:13 +0100

    test 0.2

--- demineur.js
+++ demineur.js
@@ -3,3 +3,40 @@ let enclenche = $('.enclenche')
 let non_enclenche = $('.non_enclenche')
 let mine = $('.mine')
 let nbmine = 10;
+
+let tableau = new Array();
+
+for (var i = 0; i < 10; i++) {
+
+	for (var y = 0; y < 10; y++) {
+
+		tableau[i] = new Array();
+	}
+}	
+
+
+for (var u = 0; u < 10; u++) {
+
+	for (var w = 0; w < 10; w++) {
+
+		tableau[u][w] = 0;
+	}
+}	
+
+
+
+for (var u = 0; u < 10; u++) {
+
+	for (var w = 0; w < 10; w++) {
+
+		console.log(tableau[u][w]);	}
+}	
+
+
+
+
+
+$('#'+1).html("test")
+
+
+$('.colone'+i+".ligne"+j).html();
